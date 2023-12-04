784517272462062c2b74e17ece801475512da614
Lawrence Whalen <78388882+LawrenceWhalen@users.noreply.github.com>
2023-12-04 13:37:19 -0700

    Refactor to improve orginization

---
+++ app/graphql/types/app_enums/company_type_enum.rb
@@ -0,0 +1,7 @@
+
+class Types::CompanyCategoryEnum < Types::BaseEnum
+    value "Medical", "A company offering medical services"
+    value "Equipment", "A company selling service or medical items"
+    value "Assistance", "A company employing or organizing individuals"
+    value "Service", "A company that offers a service"
+end
